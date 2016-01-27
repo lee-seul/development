50689f74a58b98bc7f28d7aff4eff82eb62a4196
seul lee <blacksangi14@naver.com>
2016-01-27 21:26:30 +1100

    no message

---
+++ python/ProjectEuler/coin_sum.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+# coin_sum.py
+
+
+coins = [1, 2, 5, 10, 20, 50, 100, 200]
+result = {i:0 for i in range(1, 201)}
+
+for coin in coins:
+	result[coin] += 1
+	for j in range(coin+1, 201):
+		result[j] += result[j-coin]
+
+print(result[200])
+
+	
+
+
+
