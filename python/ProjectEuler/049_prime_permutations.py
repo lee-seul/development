b6b6f80a9a2b6feeec8ab3ee82afe3d9631243f2
seul lee <blacksangi14@naver.com>
2016-02-09 18:45:57 +1100

    no message

--- python/ProjectEuler/049_prime_permutations.py
+++ python/ProjectEuler/049_prime_permutations.py
@@ -4,26 +4,6 @@
 from itertools import permutations as pm
 from lib.primes import is_prime
 
-
-#def no_duplication(num):
-#	num = list(str(num))
-#	num.sort()
-#	num_set = list(set(num))
-#	num_set.sort()
-#	if num == num_set:
-#		return num
-#	else:
-#		return False
-
-#def set_to_num(s):
-#	s = list(s)
-#	string = ''
-#	for x in s:
-#		string += str(x)
-#	return int(string)
-
-#l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
 def is_permutations(num1, num2, num3):
 	num1 = list(str(num1))
 	num2 = list(str(num2))
@@ -47,9 +27,6 @@ for x in primes:
 		if sub > 0 and x+sub in primes:
 			if is_permutations(y, x, x+sub):
 				print(y, x, x+sub)
-
-
-
 
 
 
