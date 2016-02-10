95ceeb11e9a4401bcd5c74d776987fed7a1c65e4
seul lee <blacksangi14@naver.com>
2016-02-10 21:22:50 +1100

    no message

---
+++ python/ProjectEuler/not/051_prime_digit_replacement.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+# 051_prime_digit_replacement.py
+
+from lib.primes import is_prime
+	
+
+
+prime_list = []
+for num in range(100000, 1000000):
+	if is_prime(num):
+		prime_list.append(num)
+
+
+
+
