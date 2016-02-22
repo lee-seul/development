4fee6c769cf7a6365103960fa52e16ed1070f181
seul lee <blacksangi14@naver.com>
2016-02-22 11:19:46 +1100

    no message

--- python/ProjectEuler/lib/gcd_lcm.py
+++ python/ProjectEuler/lib/gcd_lcm.py
@@ -1,19 +1,19 @@
 # coding: utf-8
-# gcb_lcm.py
+# gcd_lcm.py
 
 
 # 유클리드 호제법을 이용한 최소 공배수와 최대 공약수 
 
 # 최대 공약수 
-def gcb(a, b):
+def gcd(a, b):
 	if a % b == 0:
 		return b
 	else:
-		return gcb(b, a%b)
+		return gcd(b, a%b)
 
 
 # 최소 공배수
 def lcm(a, b):
-	return int(a*b/gcb(a,b))
+	return int(a*b/gcd(a,b))
 
 
--- python/ProjectEuler/lib/totient.py
+++ python/ProjectEuler/lib/totient.py
@@ -1,12 +1,12 @@
 # coding: utf-8
 # totient.py
 
-from lib.gcb_lcm import gcb
+from lib.gcd_lcm import gcd
 
 def phi(num):
 	result = 0
 	for i in range(1, num+1):
-		if gcb(num, i) == 1:
+		if gcd(num, i) == 1:
 			result += 1
 
 	return result
