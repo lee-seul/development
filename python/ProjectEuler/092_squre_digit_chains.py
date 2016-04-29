5f3fd33487b679de792e8041e5d9a4416d2e1e25
seul lee <blacksangi14@naver.com>
2016-04-29 20:33:05 +0900

    no message

--- python/ProjectEuler/092_squre_digit_chains.py
+++ python/ProjectEuler/092_squre_digit_chains.py
@@ -5,35 +5,21 @@ def digit_square_sum(num):
 	return sum([int(i)**2 for i in str(num)])
 
 
-def make_list(num):
-	limit = 9**2 * len(str(num))
-	result_list = []
-	for i in range(1, limit):
-		n = i
-		l = []
-		while(True):
-			l.append(n)
-			if n == 89 or n in result_list:
-				result_list+=l
-				break
-			elif n == 1:
-				break
-			else:
-				n = digit_square_sum(n)
-	return result_list
+def check_89(num):
+    count = 0
+    for i in range(1, num):
+        print(i)
+        result = digit_square_sum(i)
+        while(True):
+            if result == 89:
+                count +=1
+                break
+            elif result == 1:
+                break
+            else:
+                result = digit_square_sum(result)
+    return print(count)
 
-
-def count_digit_square_sum(num):
-	count = 0
-	result_list = make_list(num)
-	for i in range(1, num):
-		print(i)
-		n = digit_square_sum(i)
-		if n in result_list:
-			count+=1
-	return print(count)
-
-
-count_digit_square_sum(10000000)
+check_89(10000000)
 
 
