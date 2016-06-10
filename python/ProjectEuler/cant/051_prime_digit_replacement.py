fb8ddff5e73027574bfe53749920abfcd2fcb72f
seul lee <blacksangi14@naver.com>
2016-06-10 17:26:09 +0900

    no message

--- python/ProjectEuler/cant/051_prime_digit_replacement.py
+++ python/ProjectEuler/cant/051_prime_digit_replacement.py
@@ -2,14 +2,11 @@
 # 051_prime_digit_replacement.py
 
 from lib.primes import is_prime
-	
-
 
 prime_list = []
-for num in range(100000, 1000000):
+for num in range(100000, 10000000):
 	if is_prime(num):
 		prime_list.append(num)
-
 
 
 
