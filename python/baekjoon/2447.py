4a0e1d92b48903e6aabdb2bb041c4d4b67654d74
seul lee <blacksangi14@naver.com>
2016-11-04 21:03:52 +0900

    no message

--- python/baekjoon/2447.py
+++ python/baekjoon/2447.py
@@ -1,13 +1,11 @@
 # coding: utf-8
 
-from math import log
+from math import log, ceil
 
 n = int(input())
-k = int(log(n, 3))
+k = ceil(log(n, 3))
 matrix = [["*" for col in range(n)] for row in range(n)]
 line = ""
-if n == 243:
-    k = 5
 for cnt in range(1, k+1):
     m = 3 ** cnt; x = m//3; y = m/3 * 2
     loop = [i for i in range(0, n) if i % m >=x and i % m < y]
